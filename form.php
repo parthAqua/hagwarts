9afb14c8faa3e682410be4119202793400d69d57
parthAqua <103142141+parthAqua@users.noreply.github.com>
2022-10-04 19:21:52 +0530

    Add files via upload

---
+++ form.php
@@ -0,0 +1,30 @@
+ <?php
+ $name = $_POST['name'];
+ $visitor_email =  $_POST['email'];
+ $subject =  $_POST['subject'];
+ $message =  $_POST['message'];
+
+
+ $email_from = 'domain name';
+
+ $email_subject = 'new form submission';
+
+ $email_body = "user name: $name.\n".
+             "user email:$visitor_email.\n".
+             "subject:$subject.\n".
+             "user message:$message.\n";
+
+
+$to = 'personal email id';
+
+
+$headers = "from: $email_from \r\n ";
+
+$headers .= "reply-to: $visitor_email \r\n";
+
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("location: contact.html");
+
+ ?>
